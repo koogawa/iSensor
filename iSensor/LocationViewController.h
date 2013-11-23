135f12ef5ea9949aa732c6d11c39c2a5ea1dd4a7
koogawa <koogawa.app@gmail.com>
2013-11-23 16:40:31 +0900

    位置情報表示画面を追加

---
+++ iSensor/LocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationViewController.h
+//  iSensor
+//
+//  Created by koogawa on 2013/11/23.
+//  Copyright (c) 2013年 Kosuke Ogawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationViewController : UIViewController
+
+@end
