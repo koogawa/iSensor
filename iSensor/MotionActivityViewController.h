33823ee0e6dd69940719237945dfb62fb5d77038
koogawa <koogawa.app@gmail.com>
2013-11-26 22:52:43 +0900

    MotionActivityを追加

---
+++ iSensor/MotionActivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  MotionActivityViewController.h
+//  iSensor
+//
+//  Created by koogawa on 2013/11/26.
+//  Copyright (c) 2013年 Kosuke Ogawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MotionActivityViewController : UIViewController
+
+@end
---
+++ iSensor/MotionActivityViewController.m
@@ -0,0 +1,38 @@
+//
+//  MotionActivityViewController.m
+//  iSensor
+//
+//  Created by koogawa on 2013/11/26.
+//  Copyright (c) 2013年 Kosuke Ogawa. All rights reserved.
+//
+
+#import "MotionActivityViewController.h"
+
+@interface MotionActivityViewController ()
+
+@end
+
+@implementation MotionActivityViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
